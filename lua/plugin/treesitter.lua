f106b8c623d251609d7248d7885db0d4a591be4e
Nick Gustafson <ngustafson@microsoft.com>
2023-05-31 16:17:46 -0400

    making nvim-ts-autotag work

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,16 +1,19 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    autotag = {enable = true},
     config = function()
         require("nvim-treesitter.configs").setup{
-            ensure_installed = { 
+            ensure_installed = {
                 "lua",
                 "rust",
+                "html",
                 "javascript",
-                "typescript", 
+                "typescript",
+                "tsx",
                 "python",
             },
-            highlight  = { enable = true, }
+            highlight  = { enable = true, },
         }
     end
 }
