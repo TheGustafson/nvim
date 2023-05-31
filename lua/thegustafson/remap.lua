f67af817f08e2fc1652880c2a9524c3ef3fc5e65
Nick Gustafson <ngustafson@microsoft.com>
2023-05-31 15:26:33 -0400

    insert semicolon with <leader>; in normal mode

--- lua/thegustafson/remap.lua
+++ lua/thegustafson/remap.lua
@@ -1,7 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
 
+-- Slide with J K
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- File Tree toggle
 vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
+
+-- Insert semicolon at end
+vim.keymap.set("n", "<leader>;", "A;<esc>")
