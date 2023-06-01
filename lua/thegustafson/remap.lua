27834396ef4cc8d7666cb83d5e59599e686ddfd0
Nick Gustafson <ngustafson@microsoft.com>
2023-06-01 18:29:44 -0400

    adding <leader>, to append a , to the end of a line

--- lua/thegustafson/remap.lua
+++ lua/thegustafson/remap.lua
@@ -8,5 +8,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- File Tree toggle
 vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
 
--- Insert semicolon at end
+-- Insert semicolon at end of line
 vim.keymap.set("n", "<leader>;", "A;<esc>")
+-- Insert comma at end of line
+vim.keymap.set("n", "<leader>,", "A,<esc>")
+
